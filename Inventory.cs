d26970330bdde3b760506236288c5708e6a36821
Joey Thompson <joeythompson1014@gmail.com>
2021-03-05 07:58:52 -0500

    Initial Commit

---
+++ Inventory.cs
@@ -0,0 +1,31 @@
+﻿//Joseph Thompson
+using System;
+
+using System.IO;
+
+namespace Inventory
+{
+    class Inventory
+    {
+        static void Main(string[] args)
+        {
+            //Read Inventory Data
+            StreamReader sr = new StreamReader(@"C:\Users\joeyt\source\repos\Inventory\Inventory\Inventory.txt");
+            string data = sr.ReadLine();
+            while(data != null)
+            {
+                Console.WriteLine(data);
+                data = sr.ReadLine();
+            }
+
+            //Read Transaction Data
+            StreamReader sr2 = new StreamReader(@"C:\Users\joeyt\source\repos\Inventory\Inventory\Transactions.txt");
+            string transData = sr2.ReadLine();
+            while (transData != null)
+            {
+                Console.WriteLine(transData);
+                transData = sr2.ReadLine();
+            }
+        }
+    }
+}
---
+++ Warehouses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory
+{
+    class Warehouses
+    {
+        private string name;
+
+        public Warehouses()
+        {
+            name = "No Name";
+        }
+        public Warehouses(string name)
+        {
+            this.name = name;
+        }
+    }
+}
