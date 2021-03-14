e0e66fc70a393d629f6c030c212a6f2c36d3bdc5
Joey Thompson <joeythompson1014@gmail.com>
2021-03-14 17:16:56 -0400

    Reworked structure of program

--- Inventory.cs
+++ Inventory.cs
@@ -1,75 +1,71 @@
-//Joseph Thompson
 using System;
-
 using System.IO;
 
-namespace Inventory
+namespace Inventory_Assignment
 {
-    class Inventory
+    class Program
     {
         static void Main(string[] args)
         {
-            // Declare Global Variables
-            const char DELIM = ',';
             const string INVENTORY = "Inventory.txt";
             const string TRANSACTIONS = "Transactions.txt";
+            const char DELIM = ',';
+            Warehouse wh = new Warehouse();
+            FileStream fs = new FileStream(INVENTORY, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
 
-            //Create Warehouse Object
-            Warehouses warehouse = new Warehouses();
+            string invData;
+            invData = sr.ReadLine();
 
-            //Read Inventory.txt Data
-            FileStream file = new FileStream(INVENTORY, FileMode.Open, FileAccess.Read);
-            StreamReader sr = new StreamReader(file);
-
-            String textInput;
+            string[] whouses = {"Atlanta", "Baltimore", "Chicago", "Denver", "Ely", "Fargo"};
             string[] fields;
             Console.WriteLine("Inventory at the Beginning of the Day");
             Console.WriteLine("-------------------------------------");
-            Console.WriteLine("\n{0,-10}{1,-10}{2,-10}{3,-10}{4,-10}\n", "Part 102", "Part 215", "Part 410", "Part 525", "Part 711");
-            textInput = sr.ReadLine();
-           // Console.WriteLine("{0,-10}{1,-12}{2,-10}{3,-10}{4,-7}", "Atlanta", "Baltimore", "Chicago", "Denver", "Ely", "Fargo");
-
-           //Output Beginning Inventory Data
-            while (textInput != null)
-            {
-                fields = textInput.Split(DELIM);
-                warehouse.item1 = Convert.ToInt32(fields[0]);
-                warehouse.item2 = Convert.ToInt32(fields[1]);
-                warehouse.item3 = Convert.ToInt32(fields[2]);
-                warehouse.item4 = Convert.ToInt32(fields[3]);
-                warehouse.item5 = Convert.ToInt32(fields[4]);
-                Console.WriteLine("{0,-10}{1,-12}{2,-10}{3,-10}{4,-7}", warehouse.item1, warehouse.item2, warehouse.item3, warehouse.item4, warehouse.item5);
-                textInput = sr.ReadLine();
-            }
-
-            // Get Transaction Data
-            FileStream file2 = new FileStream(TRANSACTIONS, FileMode.Open, FileAccess.Read);
-            StreamReader sr2 = new StreamReader(file2);
-            string transData;
-            string[] fields2;
-            transData = sr2.ReadLine();
-
-            while( transData != null)
-            {
-                fields2 = transData.Split(',');
-                warehouse.transType = fields2[0];
-                warehouse.trans1 = Convert.ToInt32(fields2[1]);
-                warehouse.trans2 = Convert.ToInt32(fields2[2]);
-                transData = sr2.ReadLine();
-            }
-
-            //Output End of Day Inventory Data
             Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Inventory at the End of the Day");
-            Console.WriteLine("-------------------------------");
-            Console.WriteLine("\n{0,-10}{1,-10}{2,-10}{3,-10}{4,-10}\n", "Part 102", "Part 215", "Part 410", "Part 525", "Part 711");
-            Console.WriteLine(warehouse);
-            
+            int i = 0;
 
+            while (invData != null && i < whouses.Length)
+            {
+                fields = invData.Split(DELIM);
+                wh.item1 = Convert.ToInt32(fields[0]);
+                wh.item2 = Convert.ToInt32(fields[1]);
+                wh.item3 = Convert.ToInt32(fields[2]);
+                wh.item4 = Convert.ToInt32(fields[3]);
+                wh.item5 = Convert.ToInt32(fields[4]);
 
+                Console.WriteLine("The {0} Warehouse has:", whouses[i]);
+                Console.WriteLine("Part 102: {0}", wh.item1);
+                Console.WriteLine("Part 215: {0}", wh.item2);
+                Console.WriteLine("Part 410: {0}", wh.item3);
+                Console.WriteLine("Part 525: {0}", wh.item4);
+                Console.WriteLine("Part 711: {0}", wh.item5);
+                Console.WriteLine();
+                invData = sr.ReadLine();
+                i++;
+            }
+            fs.Close();
             sr.Close();
-            file.Close();
+
+            FileStream fs2 = new FileStream(TRANSACTIONS, FileMode.Open, FileAccess.Read);
+            StreamReader sr2 = new StreamReader(fs2);
+            string transData;
+            transData = sr2.ReadLine();
+            string[] fields2;
+            Console.WriteLine("Inventory at the End of the Day");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine();
+
+            while (transData != null)
+            {
+                if (transData == "P")
+                {
+
+                }
+                else if (transData == "S")
+                {
+
+                }
+            }
         }
     }
 }
